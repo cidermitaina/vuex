a7166590d4b12733e3d84120a83664010c0a07e8
cidermitaina <cidermitaina@gmail.com>
2018-11-06 01:16:03 +0900

    commit

---
+++ src/components/modules/HeadComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>
+        {{ title }}
+    </h1>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex'
+    export default {
+        name: 'headComp',
+        computed: mapGetters({
+            'title': 'getTitle'
+        })
+    }
+</script>
